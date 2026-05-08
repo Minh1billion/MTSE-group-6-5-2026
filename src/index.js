b0f74b21699dd43181b76082a0d23dd86553c9eb
TranAnhHuy <anhhuyrubic@gmail.com>
2026-05-08 20:39:10 +0700

    style: standardize import and code formatting in index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import 'dotenv/config';
-import express from 'express';
-import { connectDB } from './config/db.js';
-import sequelize from './config/db.js';
-import authRouter from './modules/auth/auth.router.js';
+import "dotenv/config";
+import express from "express";
+import { connectDB } from "./config/db.js";
+import sequelize from "./config/db.js";
+import authRouter from "./modules/auth/auth.router.js";
+import userRouter from "./modules/user/user.router.js";
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/auth', authRouter);
-
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 const start = async () => {
-  try {
-    await connectDB();
+    try {
+        await connectDB();
+        await sequelize.sync({ alter: true });
+        console.log("Database synced");
 
-    await sequelize.sync({ alter: true });
-    console.log('Database synced');
-
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
-  } catch (err) {
-    console.error('Startup error:', err);
-    process.exit(1);
-  }
+        app.listen(process.env.PORT, () =>
+            console.log(`Server running on port ${process.env.PORT}`),
+        );
+    } catch (err) {
+        console.error("Startup error:", err);
+        process.exit(1);
+    }
 };
 
 start();
